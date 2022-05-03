bfad96ceb194bf38d53205fe3ead2d4c5553edb4
Keven Bouchard <peckgfx@gmail.com>
2022-05-03 11:50:07 -0400

    asd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./Home";
-import MessyComponent from "./MessyComponent";
-import Wrapper from "./Wrapper";
+//import Home from "./Home";
+//import MessyComponent from "./MessyComponent";
+//import Wrapper from "./Wrapper";
 
 import "./styles.css";
 
 export default function App() {
   return "test";
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Wrapper />}>
-          <Route index element={<Home />} />
-          <Route path="/messy" element={<MessyComponent />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+
+  // return (
+  //   <BrowserRouter>
+  //     <Routes>
+  //       <Route path="/" element={<Wrapper />}>
+  //         <Route index element={<Home />} />
+  //         <Route path="/messy" element={<MessyComponent />} />
+  //       </Route>
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
 }
